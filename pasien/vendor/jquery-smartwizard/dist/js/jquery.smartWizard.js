f2268640ef46d806effee3cd9263575d145cbb80
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-11-27 16:22:25 +0700

    update buat janji

--- pasien/vendor/jquery-smartwizard/dist/js/jquery.smartWizard.js
+++ pasien/vendor/jquery-smartwizard/dist/js/jquery.smartWizard.js
@@ -109,8 +109,8 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
     },
     lang: {
       // Language variables for button
-      next: 'Next',
-      previous: 'Previous'
+      next: 'Berikutnya',
+      previous: 'Sebelumnya'
     },
     disabledSteps: [],
     // Array Steps disabled
